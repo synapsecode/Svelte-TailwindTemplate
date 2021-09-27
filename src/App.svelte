0d690be3617896a7cd5be1481c0a88f45f464f60
Manas Hejmadi <manas2all@outlook.com>
2021-09-28 02:05:46 +0530

    InitialCommit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<body class="flex items-center justify-center h-screen">
+  <div class="text-gray-900 font-bold">
+    <div class="flex">
+      <img
+        class="object-contain h-48 w-full z-50"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/996px-Svelte_Logo.svg.png"
+        alt="f"
+      />
+      <img
+        class="object-contain h-48 w-full -ml-60"
+        src="https://miro.medium.com/max/632/1*5QD8DKhOjRe-gcYjozlLNQ.png"
+        alt="f"
+      />
+    </div>
+    <p class="text-6xl mt-8 ml-6">Svelte + TailwindCSS</p>
+    <p class="text-2xl mt-8 text-gray font-light -mt-0">
+      Unleash the full potential of your Svelte app using TailwindCSS
+    </p>
+  </div>
+</body>
+
+<style lang="postcss" global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
